98e96873637b3fe527d0dc44dc3ac2c1904a1391
jarosik10 <bartosz.jaros96@gmail.com>
2021-01-20 09:40:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const displayPopup = (el) => {
+    el.style.display = 'block';
+}
+
+const hidePopup = (el) => {
+    el.style.display = 'none';
+}
+
+const defaultExportFunction = () => {
+    const popUp = document.getElementById('js-popup');
+
+    if (!popUp) { return; }
+
+    const popUpCloseButton = document.querySelector('.popup-close');
+    popUpCloseButton.addEventListener('click', () => hidePopup(popUp));
+
+    // cookie - counter cookie name, hideO
+    const { cookie, hideOn, showAfter, showMax } = popUp.dataset;
+    console.log(cookie, hideOn, showAfter, showMax)
+
+    const cookieCounter =  Number(window.localStorage.getItem(cookie));
+    const locationPath = window.location.pathname;
+
+    const hideOnArray = hideOn.split(',');
+
+    if (cookieCounter < Number(showMax) && !hideOnArray.includes(locationPath)) {
+        window.localStorage.setItem(cookie, cookieCounter + 1);
+        setTimeout(() => displayPopup(popUp), showAfter);
+    } else {
+        alert(`cookieCounter is ${cookieCounter} >= ${showMax} or ${locationPath} is one of: ${hideOn}`);
+    }
+}
+
+
+defaultExportFunction();
+
+const mainButton = document.querySelector('.main-button');
+mainButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('Form sent');
+})
+
+
